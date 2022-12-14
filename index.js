63b3169a1234ab3f733cd806080e7879f1b26551
Annika Kriskovich <annika.krisko2013@gmail.com>
2022-12-14 14:57:05 -0600

    commit tree 34d41a21bd073b0f0e600daf0546df63d892cf1a

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+   cats.push("Ralph")
+   return cats
+};
+
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob")
+    return cats
+};
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+    return cats
+};
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+    return cats
+}
+
+function appendCat(name) {
+    const copyOfCats = [...cats,"Broom"]
+    return copyOfCats
+}
+
+function prependCat(name) {
+    const catss = ["Arnold", ...cats]
+    return catss
+}
+
+function removeLastCat() {
+    const catts = cats.slice(0,cats.length-1)
+    return catts
+}
+
+function removeFirstCat() {
+    const caats = cats.slice(1)
+    return caats
+}
